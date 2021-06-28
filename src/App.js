6d38b1bd13c55730374d2a48d1bd5ad4ba103a80
rhiasoni <50022366+rhiasoni@users.noreply.github.com>
2021-06-28 15:17:11 -0400

    add chokidar

--- src/App.js
+++ src/App.js
@@ -54,4 +54,11 @@ const App = () => {
   );
 }
 
+const chokidar = require('chokidar');
+
+// One-liner for current directory
+chokidar.watch('.').on('all', (event, path) => {
+  console.log(event, path);
+});
+
 export default App;
