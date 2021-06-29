1953e7e136a49e759dd83d7ccb355fcb10af223a
rhiasoni <50022366+rhiasoni@users.noreply.github.com>
2021-06-29 13:08:01 -0400

    reverted files to original create-react-app from render

--- src/App.js
+++ src/App.js
@@ -54,6 +54,12 @@ const App = () => {
   );
 }
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 const chokidar = require('chokidar');
 
 // One-liner for current directory
